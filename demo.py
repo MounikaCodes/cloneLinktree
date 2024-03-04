4f97b7a79b436aef43b6a446f657ad3ccc1f0bd3
MounikaCodes <mounikalogins@gmail.com>
2024-03-04 22:17:55 +0530

    clone ready

---
+++ demo.py
@@ -0,0 +1,31 @@
+# HR executive tasks in EMPLOYEE MANAGEMENT
+
+
+dataset = [
+    {"name":"mounika","id":"EMP001","age":23,"gender":"Female","salary":30000,"tech_known":["python","JS","java","HTML","CSS"]},
+    {"name":"sai","id":"EMP002","age":24,"gender":"Male","salary":35000,"tech_known":["python"]},
+    {"name":"vijay","id":"EMP003","age":24,"gender":"Male","salary":20000,"tech_known":["JS","HTML","CSS"]},
+    {"name":"shiva","id":"EMP004","age":25,"gender":"Male","salary":32000,"tech_known":["python","JS","HTML","CSS"]},
+    {"name":"hemanth","id":"EMP005","age":26,"gender":"Male","salary":34000,"tech_known":["python","JS"]},
+    {"name":"pooja","id":"EMP006","age":22,"gender":"Female","salary":29000,"tech_known":["HTML","CSS"]},
+]
+
+print(dataset)
+#  1. count number of male and no of females
+#  2. Calculate total salaries
+#  3. give hike of 2000 to the employee whose age is less than 23
+#  4. give hike of 5% to the employees whose age = 25
+#  5. find sum of female employees salary and male employees salary seperately
+#  6. find names of employees who knows atleast 2 technologies
+#  7. find employees who knows python, JS, java,HTML,CSS give them hike of 15%
+#  8. find the NAME OF employee who is having highest salary
+#  9. find the employees who knows only one technology and remove them
+#  10. add new emoployee name = Gulabjan, id = EMP007,gender = Female , salary = 32000, tech_known = powerbi, python
+
+# ----------------------------------------------------------------------------------------
+
+
+
+
+#  1. push all the data into excel using openpyxl
+#  2. do visualisation based on their salaries
